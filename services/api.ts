51c49ceefca60b191b1969b9a3514ac33a9af83b
Alexsandro Urbano <99767577+AlegSandrin@users.noreply.github.com>
2023-10-16 16:24:57 -0300

    Configurando variavel de ambiente para deploy no Vercel

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.NEXTAUTH_URL || ""}/api`,
+  baseURL: `${process.env.NEXTAUTH_URL || process.env.VERCEL_URL}/api`,
 });
 
 export default api;
