99f0007fd056177f0e385eb0cd91e4496d8084b6
Alexsandro Urbano <99767577+AlegSandrin@users.noreply.github.com>
2023-10-16 17:40:13 -0300

    **Correção da rota da api

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const route = process.env.NEXTAUTH_URL ? process.env.NEXTAUTH_URL : process.env.VERCEL_URL;
+const route = process.env.NEXTAUTH_URL ? process.env.NEXTAUTH_URL : "";
 
 const api = axios.create({
   baseURL: `${route}/api`,
