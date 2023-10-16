8b626d1b719e8fefcff9674d2f8d7ba65197f006
Alexsandro Urbano <99767577+AlegSandrin@users.noreply.github.com>
2023-10-16 17:20:22 -0300

    Mais correções

--- lib/getLocalTodoID.ts
+++ lib/getLocalTodoID.ts
@@ -1,6 +1,11 @@
 import { ITodo } from "@/types/TodoType";
 
-export function getLocalTodoID(todos: ITodo[]) {
+export function getLocalTodoID(todos: ITodo[] | null | undefined) {
+    if(typeof window === "undefined") return [];
+    if(!todos) {
+        localStorage.setItem("todos", JSON.stringify([]));
+        return;
+    }
     const mappedTodos = todos.map((todo, index) => ({...todo, _id: index}));
     return mappedTodos;
 }
